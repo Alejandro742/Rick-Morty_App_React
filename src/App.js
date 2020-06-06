b070256c7c5fd637a0361d0099422512e1a0da6a
Alejandro Barba <alejandrobarba742@gmail.com>
2020-06-05 19:21:54 -0500

    estructura de la vista

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>asas</h1>
+    <main className = 'container text-center'>
+      <h1>Personajes Rick & Morty</h1>
+      <div className="row">
+        <div className="col-md-5">
+          <div className="card">
+            <div className="card-body">
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-5">
+          <div className="card">
+            <div className="card-body">
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main>
   );
 }
 
