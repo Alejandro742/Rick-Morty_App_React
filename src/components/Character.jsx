0fde5b64c95c5c63bc5ea2d9aff688135a19c8f9
Alejandro Barba <alejandrobarba742@gmail.com>
2020-06-05 21:44:22 -0500

    componente para mostrar el personaje

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Fragment, useState } from "react";
 
+const Character = ({ character }) => {
+  //character state
 
-const Character = () => {
-    return ( 
-        <h2>Personaje</h2>
-     );
-}
- 
+  return (
+    <Fragment>
+      <p>
+        Nombre: <span id="nombre-span">{character.name}</span>
+      </p>
+      <p>
+        Estado: <span id="estado-span">{character.status}</span>
+      </p>
+      <p>
+        Género: <span id="especie-span">{character.gender}</span>
+      </p>
+      <p>
+        Especie: <span id="especie-span">{character.species}</span>
+      </p>
+      <img src={character.image} alt="imagen_personaje"/>
+    </Fragment>
+  );
+};
+
 export default Character;
