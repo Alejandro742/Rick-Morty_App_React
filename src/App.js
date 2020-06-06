f26e242a59bc51537d2ace2bffced81b8d9d4050
Alejandro Barba <alejandrobarba742@gmail.com>
2020-06-05 19:31:54 -0500

    Estructura base redefinida

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import Form from './components/Form';
+import Character from './components/Character';
 
 function App() {
+  // 
   return (
     <main className = 'container text-center'>
       <h1>Personajes Rick & Morty</h1>
       <div className="row">
         <div className="col-md-5">
           <div className="card">
+            <div className="card-header">
+              Formulario
+            </div>
             <div className="card-body">
-
+              <Form
+                
+              />
             </div>
           </div>
         </div>
-        <div className="col-md-5">
+        <div className="col-md-7">
           <div className="card">
+            <div className="card-header">
+              Personaje
+            </div>
             <div className="card-body">
+              <Character 
 
+              />
             </div>
           </div>
         </div>
