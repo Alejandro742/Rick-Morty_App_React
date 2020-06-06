6bc91944cb562e9ed40e89102de9892fee5ba3f5
Alejandro Barba <alejandrobarba742@gmail.com>
2020-06-05 20:59:42 -0500

    component form completo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,33 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useState} from 'react';
 
 
 
-const Form = () => {
+const Form = ({saveName}) => {
 
-    const submitName = () =>{
-        console.log('Enviando')
+    //state para obtener nombre del form
+    const [name,getName] = useState('');
+    // state para manejar error
+    const [error,updateError] = useState(false);
+
+    const actualizarState = (e) =>{
+        getName(e.target.value);
     }
+
+    const submitName = (e) =>{
+        e.preventDefault();
+        //verofy if the form is not empty
+        if(name.trim()===''){
+            updateError(true);
+            return;
+        }
+        //clear error state
+        updateError(false);
+
+        //save name in app hooks
+        saveName(name);
+        //clean form
+        getName('');
+    };
 
     return ( 
         <Fragment>
@@ -15,7 +36,12 @@ const Form = () => {
             >
                 <div className="form-group">
                     <label htmlFor="personaje">Nombre del Personaje</label>
-                    <input type="text" id="personaje" className="form-control fntsz-secundary" autoCorrect placeholder="Nombre del Personaje"/>
+                    <input type="text" name="name" className="form-control fntsz-secundary"
+                    autoFocus placeholder="Nombre del Personaje"
+                    onChange={actualizarState}
+                    value = {name}
+                    
+                    />
                 </div>
                 <div className="form-group">
                     <button
