1c016b2de1975b9ee9d5fa0b7e50fd04c44fe0cf
Alejandro Barba <alejandrobarba742@gmail.com>
2020-06-05 22:23:42 -0500

    componente mensaje de error

---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mensaje = ({msg}) => {
+    return ( 
+        <div className="alert alert-danger p-2 mb-2 mt-2 fntsz-third">
+            {msg}
+        </div>
+     );
+}
+ 
+export default Mensaje;
